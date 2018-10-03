ab965f40326ee8eaf2efdff5c35f5cf06f653292
zhuchengxi <448712280@qq.com>
2018-10-03 10:28:07 +0800

    绘制地图

--- src/main/webapp/WEB-INF/views/userLog.jsp
+++ src/main/webapp/WEB-INF/views/userLog.jsp
@@ -12,6 +12,7 @@
     <link href="/css/style.css" type='text/css' rel="stylesheet"/>
     <script type="text/javascript" src='/js/jquery.js'></script>
     <script type="text/javascript" src='/js/echarts.min.js'></script>
+    <script type="text/javascript" src='/js/china.js'></script>
     <script type="text/javascript" src='/js/custom.js'></script>
 </head>
 <body>
@@ -20,21 +21,21 @@
         <div class="title">所有买家各消费行为对比</div>
         <div class="show">
             <div class='chart-type'>饼图</div>
-            <div id="main" style="height:400px"></div>
+            <div id="main" style="width: 1000px;height: 600px;"></div>
         </div>
     </div>
     <div>
         <div class="title">男女买家交易对比</div>
         <div class="show">
             <div class='chart-type'>饼图</div>
-            <div id="main1" style="height:400px"></div>
+            <div id="main1" style="width: 1000px;height: 600px;"></div>
         </div>
     </div>
     <div>
         <div class="title">男女买家各个年龄段交易对比</div>
         <div class="show">
             <div class='chart-type'>散点图</div>
-            <div id="main2" style="height:400px"></div>
+            <div id="main2" style="width: 1000px;height: 600px;"></div>
         </div>
     </div>
 
@@ -42,7 +43,7 @@
         <div class="title">商品类别交易额对比</div>
         <div class="show">
             <div class='chart-type'>饼图</div>
-            <div id="main3" style="height:400px"></div>
+            <div id="main3" style="width: 1000px;height: 600px;"></div>
         </div>
     </div>
 
@@ -50,7 +51,7 @@
         <div class="title">各省份的总成交量对比</div>
         <div class="show">
             <div class='chart-type'>地图</div>
-            <div id="main5" style="height:400px"></div>
+            <div id="main4" style="width: 1000px;height: 600px;"></div>
         </div>
     </div>
 </div>
--- src/main/webapp/js/custom.js
+++ src/main/webapp/js/custom.js
@@ -8,7 +8,7 @@ $(function () {
             SexCompareChart.getChart(data.Chart1List);
             AgeCompareChart.getChart(data.Chart2List);
             TradingvolumeChart.getChart(data.Chart3List);
-            // ProvinceChart.getChart(data.Chart4List);
+            ProvinceChart.getChart(data.Chart4List);
         }
     });
 
@@ -318,6 +318,89 @@ var TradingvolumeChart = {
 var ProvinceChart = {
     getChart: function (dataSource) {
         var myChart = echarts.init(document.getElementById('main4'));
+
+        var data = [];
+        for (var i = 0; i < dataSource.length; i++) {
+            var tmp = {};
+            tmp.name = dataSource[i].province;
+            tmp.value = dataSource[i].num;
+            data.push(tmp);
+        }
+
+        option = {
+            title: {
+                text: "不同地区购物情况",
+                x: 'center'
+            },
+            tooltip: { //tooltip提示框，鼠标悬浮交互时的信息提示
+                trigger: 'item' //触发类型，默认数据触发，见下图，可选为：'item' | 'axis'
+            },
+            legend: { //legend图例
+                orient: 'vertical', //布局方式，默认为水平布局，可选为：'horizontal' | 'vertical'
+                x: 'left',
+                data: ['购买量'] //图例内容数组
+            },
+            dataRange: { //dataRange值域选择
+                min: 0, //指定的最小值，eg: 0，默认无，必须参数，唯有指定了splitList时可缺省min。
+                max: 2500,
+                x: 'left', //水平安放位置，默认为全图左对齐，可选为：'center' | 'left' | 'right' | {number}（x坐标，单位px）
+                y: 'bottom', //垂直安放位置，默认为全图底部，可选为：'top' | 'bottom' | 'center' | {number}（y坐标，单位px）
+                text: ['高', '低'], // 文本，默认为数值文本
+                calculable: true //是否启用值域漫游，启用后无视splitNumber和splitList，值域显示为线性渐变
+            },
+            toolbox: { //toolbox
+                show: true,
+                orient: 'vertical', //布局方式，默认为水平布局，可选为：'horizontal' | 'vertical'
+                x: 'right',
+                y: 'center',
+                feature: {
+                    mark: {
+                        show: true
+                    },
+                    dataView: {
+                        show: true,
+                        readOnly: false
+                    },
+                    restore: {
+                        show: true
+                    },
+                    saveAsImage: {
+                        show: true
+                    }
+                }
+            },
+            roamController: { //缩放漫游组件
+                show: true,
+                x: 'right',
+                mapTypeControl: { //必须，指定漫游组件可控地图类型，如：{ china: true }
+                    'china': true
+                }
+            },
+            //series : eval("[" + dataStr + "]")
+            series: [ //通用,驱动图表生成的数据内容数组，数组中每一项为一个系列的选项及数据，其中个别选项仅在部分图表类型中有效
+                {
+                    name: '购买量',
+                    type: 'map',
+                    mapType: 'china',
+                    itemStyle: {
+                        normal: {
+                            label: {
+                                show: true
+                            }
+                        },
+                        emphasis: {
+                            label: {
+                                show: true
+                            }
+                        }
+                    },
+                    data: data
+                }
+            ]
+        };
+
+        // 为echarts对象加载数据
+        myChart.setOption(option);
 
 
     }
